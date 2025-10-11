1d9b3a5d3d06d702985b4670c6ea5f60a16d1f39
vasyl-ks <vasylks@outlook.es>
2025-10-11 22:51:27 +0200

    refactor(consumer/logger): implement multiple scoped loggers with rotation
    Refactored the logging system to replace the single global logger with
    dedicated loggers for main, data, and command outputs. Each logger now
    writes to its own directory and automatically rotates files after
    reaching the configured line limit.

--- internal/consumer/logger.go
+++ internal/consumer/logger.go
@@ -2,12 +2,73 @@ package consumer
 
 import (
 	"fmt"
-	"os"
-	"time"
 	"log"
+	"os"
+	"path/filepath"
+	"time"
+
 	"github.com/vasyl-ks/TM-software-H11/config"
 	"github.com/vasyl-ks/TM-software-H11/internal/model"
 )
+
+// Helper type for grouped loggers
+type Loggers struct {
+	Main    *log.Logger
+	Data    *log.Logger
+	Command *log.Logger
+}
+
+// Helper function to create a logger for a given subdirectory and prefix
+func createLogger(baseDir, subDir, prefix string) (*log.Logger, *os.File, error) {
+	dirPath := filepath.Join(baseDir, subDir)
+	if err := os.MkdirAll(dirPath, 0755); err != nil {
+		return nil, nil, fmt.Errorf("error creating directory %s: %w", dirPath, err)
+	}
+
+	filename := fmt.Sprintf("%s_%s.jsonl", prefix, time.Now().Format("20060102_150405"))
+	fullPath := filepath.Join(dirPath, filename)
+
+	file, err := os.OpenFile(fullPath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error opening log file %s: %w", fullPath, err)
+	}
+
+	logger := log.New(file, "", 0)
+	return logger, file, nil
+}
+
+// Helper function to write a ResultData
+func writeResult(loggers Loggers, r model.ResultData) {
+	msg := fmt.Sprintf(
+		"[DATA] Created at %s, Processed at %s, Logged at %s | "+
+			"AvgSpeed: %5.2f, MinSpeed: %5.2f, MaxSpeed: %5.2f | "+
+			"AvgTemp: %5.2f, MinTemp: %5.2f, MaxTemp: %5.2f | "+
+			"AvgPressure: %4.2f, MinPressure: %4.2f, MaxPressure: %4.2f",
+		r.CreatedAt.Format("15:04:05.000000"),
+		r.ProcessedAt.Format("15:04:05.000000"),
+		time.Now().Local().Format("15:04:05.000000"),
+		r.AverageSpeed, r.MinSpeed, r.MaxSpeed,
+		r.AverageTemp, r.MinTemp, r.MaxTemp,
+		r.AveragePressure, r.MinPressure, r.MaxPressure,
+	)
+
+	loggers.Main.Println(msg)
+	loggers.Data.Println(msg)
+}
+
+// Helper function to write a Command
+func writeCommand(loggers Loggers, cmd model.Command) {
+	msg := fmt.Sprintf(
+		"[COMMAND] Received at %s | Action: %-12s | Params: %-8v",
+		time.Now().Local().Format("15:04:05.000000"),
+		cmd.Action,
+		cmd.Params,
+	)
+
+	loggers.Main.Println(msg)
+	loggers.Command.Println(msg)
+}
+
 
 /*
 Log receives ResultData and Command messages from their respective channels
@@ -22,23 +83,40 @@ func Log(inResultChan <-chan model.ResultData, inCommandChan <-chan model.Comman
 	fileDir := config.Logger.FileDir // defines directory where the log is saved.
 	maxLines := config.Logger.MaxLines // defines the maximum number of ResultData to log in a single file.
 
-	// Check directory
-	err := os.MkdirAll(fileDir, 0755)
-	if err != nil {
+	// Create base directory
+	if err := os.MkdirAll(fileDir, 0755); err != nil {
 		fmt.Println("Error creating directory:", err)
 		return
 	}
 
-	// Create a new file
-	filename := fmt.Sprintf("%s/sensor_log_%s.jsonl", fileDir, time.Now().Format("20060102_150405"))
-	file, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+	// Create all loggers
+	mainLogger, mainFile, err := createLogger(fileDir, "", "log")
 	if err != nil {
-		fmt.Println("Error opening log file:", err)
+		fmt.Println(err)
 		return
 	}
-	defer file.Close()
-	log.SetOutput(file)
-	log.SetFlags(0)
+	defer mainFile.Close()
+
+	dataLogger, dataFile, err := createLogger(fileDir, "data", "data")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer dataFile.Close()
+
+	commandLogger, commandFile, err := createLogger(fileDir, "commands", "command")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer commandFile.Close()
+
+	// Group them for easier access
+	loggers := Loggers{
+		Main:    mainLogger,
+		Data:    dataLogger,
+		Command: commandLogger,
+	}
 
 	for {
 		select {
@@ -49,28 +127,17 @@ func Log(inResultChan <-chan model.ResultData, inCommandChan <-chan model.Comman
 				continue
 			}
 			// Log in the file
-       		log.Printf(
-        	    "[DATA] || Created at %s, Processed at %s, Logged at %s | AvgSpeed: %5.2f, MinSpeed: %5.2f, MaxSpeed: %5.2f | AvgTemp: %5.2f, MinTemp: %5.2f, MaxTemp: %5.2f | AvgPressure: %4.2f, MinPressure: %4.2f, MaxPressure: %4.2f ||\n",
-        	    resultData.CreatedAt.Format("15:04:05.000000"),
-        	    resultData.ProcessedAt.Format("15:04:05.000000"),
-        	    time.Now().Local().Format("15:04:05.000000"),
-        	    resultData.AverageSpeed, 	resultData.MinSpeed, 	resultData.MaxSpeed,
-        	    resultData.AverageTemp, 	resultData.MinTemp, 	resultData.MaxTemp,
-        	    resultData.AveragePressure, resultData.MinPressure, resultData.MaxPressure,
-			)
+       		writeResult(loggers, resultData)
 
-			// Receive ResultData
+
+			// Receive Command
 			case cmd, ok := <-inCommandChan:
 			if !ok {
 				inCommandChan = nil // channel closed
 				continue
 			}
 			// Log in the file
-			log.Printf("[COMMAND] || Received at %s | Action: %s | Params: %+v ||",
-				time.Now().Local().Format("15:04:05.000000"),
-				cmd.Action,
-				cmd.Params,
-			)
+			writeCommand(loggers, cmd)
 		}
 
 		// Exit if both channels are closed
@@ -80,18 +147,36 @@ func Log(inResultChan <-chan model.ResultData, inCommandChan <-chan model.Comman
 
 		lineCount++
 		if lineCount >= maxLines {
-			file.Close()
+			mainFile.Close()
+			dataFile.Close()
+			commandFile.Close()
 			
 			// Create a new file
-			filename := fmt.Sprintf("%s/sensor_log_%s.jsonl", fileDir, time.Now().Format("20060102_150405"))
-			file, err = os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+			mainLogger, mainFile, err = createLogger(fileDir, "", "log")
 			if err != nil {
-				fmt.Println("Error opening new log file:", err)
+				fmt.Println(err)
 				return
 			}
-			defer file.Close()
-			log.SetOutput(file)
 
+			dataLogger, dataFile, err = createLogger(fileDir, "data", "data")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			commandLogger, commandFile, err = createLogger(fileDir, "commands", "command")
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+
+			// Group them for easier access
+			loggers = Loggers{
+				Main:    mainLogger,
+				Data:    dataLogger,
+				Command: commandLogger,
+			}
+				
 			lineCount = 0
 		}
 	}
